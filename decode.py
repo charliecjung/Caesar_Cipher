898a3590e12bf7a80e090253fdb19996791020cd
Charlie Jung <32102793+Charlanx@users.noreply.github.com>
2018-06-06 19:53:14 -0500

    initial commit

---
+++ decode.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import string
+userMessage = raw_input("Enter the message you would like to decrypt: ")
+shiftNum = int(raw_input("Enter the shift number: "))
+alphabet = string.printable 
+finalMessage = ""
+
+for x in xrange(0, len(userMessage)):
+   if (userMessage[x] == " "):
+       finalMessage += " "
+       continue;
+
+   newShiftNum = (alphabet.index(userMessage[x]) - shiftNum) % len(alphabet) 
+   """
+   Debugging Print Statements
+   print ("Decoding your message...")
+   print ("Position of letter[" + userMessage[x] + "]: " + str(alphabet.index(userMessage[x]))) 
+   print ("ShiftNum: " + str(newShiftNum))
+   """
+   newChar = alphabet[newShiftNum]
+   finalMessage += newChar 
+   
+
+print ("Your Final Message is: " + finalMessage)
